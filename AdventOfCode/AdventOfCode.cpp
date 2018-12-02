98c86db33121507caebde59b868c304a4d1bd897
Masterchoc <mel.florance@gmail.com>
2018-12-02 20:42:22 +0100

    Create initial project

--- AdventOfCode/AdventOfCode.cpp
+++ AdventOfCode/AdventOfCode.cpp
@@ -1,21 +1,26 @@
-// AdventOfCode.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
-
 #include "pch.h"
-#include <iostream>
+
+std::string dayOne()
+{
+	std::string line;
+	std::ifstream file("day1.txt");
+
+	if (!file.is_open())
+		return NULL;
+
+	while (getline(file, line))
+	{
+		std::cout << line << '\n';
+	}
+
+	file.close();
+
+	return "ok";
+}
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	std::cout << "Advent of code 2018" << std::endl;
+	std::cout << "===================" << std::endl;
+	std::cout << "Day 1: " << dayOne() << std::endl;
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Conseils pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
